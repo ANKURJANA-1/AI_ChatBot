f62611657d19f0f90d3f6d0dcd06fa63ca5e421c
ANKURJANA-1 <ankurjana905@gmail.com>
2021-07-31 11:57:17 +0530

    project initial setup

---
+++ app/src/main/java/com/mobile/android/chatbot/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mobile.android.chatbot.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mobile.android.chatbot.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
